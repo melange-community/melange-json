9f56e03b1a0beaa3625564af44b26c6381b3c293
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-09-11 17:39:19 -0700

    feat: port to Melange v2

--- examples/decode.ml
+++ examples/decode.ml
@@ -1,29 +1,28 @@
 (* Decoding a fixed JSON data structure using Json.Decode *)
-let mapJsonObjectString f decoder (encoder: int -> Js.Json.t) str =
+let mapJsonObjectString f decoder (encoder : int -> Js.Json.t) str =
   let json = Json.parseOrRaise str in
   Json.Decode.(dict decoder json)
-    |> Js.Dict.map ((fun v -> f v) [@bs])
-    |> Json.Encode.dict encoder
-    |> Json.stringify
+  |> Js.Dict.map (fun [@u] v -> f v)
+  |> Json.Encode.dict encoder |> Json.stringify
 
-let sum =
-  Array.fold_left (+) 0
+let sum = Array.fold_left ( + ) 0
 
 (* prints `{ "foo": 6, "bar": 24 }` *)
 let _ =
-  Js.log @@
-    (mapJsonObjectString sum Json.Decode.(array int) Json.Encode.int {|
+  Js.log
+  @@ mapJsonObjectString sum
+       Json.Decode.(array int)
+       Json.Encode.int
+       {|
       {
         "foo": [1, 2, 3],
         "bar": [9, 8, 7]
       }
-    |})
+    |}
 
 (* Error handling *)
 let _ =
   let json = {|{ "y": 42 } |} |> Json.parseOrRaise in
   match Json.Decode.(field "x" int json) with
-  | x ->
-    Js.log x
-  | exception Json.Decode.DecodeError msg ->
-    Js.log ("Error:" ^ msg)
+  | x -> Js.log x
+  | exception Json.Decode.DecodeError msg -> Js.log ("Error:" ^ msg)
--- src/Json.ml
+++ src/Json.ml
@@ -3,17 +3,14 @@ module Encode = Json_encode
 
 exception ParseError of string
 
-let parse s =
-  try Some (Js.Json.parseExn s) with
-  | _ -> None
+let parse s = try Some (Js.Json.parseExn s) with _ -> None
 
 let parseOrRaise s =
-  try Js.Json.parseExn s with
-  | Js.Exn.Error e ->
+  try Js.Json.parseExn s
+  with Js.Exn.Error e ->
     let message =
-      match Js.Exn.message e with
-      | Some m  -> m
-      | None    -> "Unknown error"
-    in raise @@ ParseError message
+      match Js.Exn.message e with Some m -> m | None -> "Unknown error"
+    in
+    raise @@ ParseError message
 
-external stringify : Js.Json.t -> string = "JSON.stringify" [@@bs.val]
+external stringify : Js.Json.t -> string = "JSON.stringify"
