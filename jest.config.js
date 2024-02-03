5f884a577dd3b7064fc3f2f64cd2362d3c0f7e74
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-02-03 15:11:03 -0800

    fix: tests for new Node.js parsers, unify jest configs (#7)
    * fix: tests for new Node.js parsers, unify jest configs
    
    * chore: require dune 3.9

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    rootDir: "./_build/default/",
-    testMatch: ["**/*_test.js"],
+  rootDir : "./_build/default/",
+  testMatch : [ "**/*_test.js" ],
+  testEnvironment : "node"
 };
-  
--- src/__tests__/Json_test.ml
+++ src/__tests__/Json_test.ml
@@ -17,6 +17,15 @@ let _ =
           try
             let (_ : Js.Json.t) = parseOrRaise "{" in
             fail "should throw"
-          with ParseError "Unexpected end of JSON input" -> pass));
+          with
+          | ParseError "Unexpected end of JSON input"
+          | ParseError (* Node.js v20 *)
+              "Expected property name or '}' in JSON at position 1"
+          | ParseError
+              (* Node.js v21 *)
+              "Expected property name or '}' in JSON at position 1 (line 1 \
+               column 2)"
+          ->
+            pass));
 
   test "stringify" (fun () -> expect @@ stringify Encode.null |> toEqual "null")
